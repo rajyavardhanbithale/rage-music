89a8c79c9af5ea3750f1b8c2a43df9a72d7dba8a
rajyavardhan bithale <70558897+rajyavardhanbithale@users.noreply.github.com>
2022-12-01 21:29:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+'''
+import pafy
+
+url = 'https://youtu.be/7dSDoh9ebVA'
+video = pafy.new(url)
+
+#print(video)
+
+print("\n")
+#streams = video.audiostreams
+
+#for s in streams:
+#    print(s.bitrate, s.extension,s.get_filesize())
+
+
+bestaudio = video.getbestaudio()
+print(bestaudio.url)
+
+'''
+
+'''from youtubesearchpython import VideosSearch
+
+
+#print(x['result'][0]['richThumbnail']['url'])
+
+videosSearch = VideosSearch('skyfall', limit = 2)
+
+song_search_youtube = videosSearch.result()
+
+result = song_search_youtube['result'][0]
+
+
+
+print(result ['channel']['name'])
+print(result['thumbnails'][0]['url'])
+print(result['link'])'''
+
+
+import pafy
+
+url = 'https://www.youtube.com/watch?v=lnRLg9ouVHc&ab_channel=T3NZU'
+video = pafy.new(url)
+
+#print(video)
+
+print("\n")
+#streams = video.audiostreams
+
+#for s in streams:
+#    print(s.bitrate, s.extension,s.get_filesize())
+
+
+bestaudio = video.getbestaudio()
+streamSongUrl =  bestaudio.url
+
+print(streamSongUrl)
